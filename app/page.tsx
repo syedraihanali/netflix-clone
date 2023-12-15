6e5577b1de066589ebcac210124420c986ffb7af
Syed Raihan Ali <syedraihanali65@gmail.com>
2023-12-15 13:52:27 +0000

    setup complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-      <h1 className='text-red font-bold'>
-        Hello World!!
-      </h1>
+      <Button>Hello World</Button>
     </main>
   )
 }
+
